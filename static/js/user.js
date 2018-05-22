1ed36ece2bd98623ec75c54344ed0aca9e214726
cwg <cuiwenguang@gsoft.cc>
2018-05-22 18:10:17 +0800

    用户列表

--- static/js/user.js
+++ static/js/user.js
@@ -2,7 +2,8 @@ $(function () {
     $("#tableUser").bootstrapTable({
         "url": "/system/getusers"
     });
-
+    validate();
+    $("#btnSave").click(save);
 });
 
 function showEdit(id) {
@@ -12,20 +13,56 @@ function showEdit(id) {
     $("#frmCreate").modal();
 }
 
-function save() {
-    
+var save = function() {
+    $("#frmUser").bootstrapValidator('validate');
+    if($("#frmUser").data('bootstrapValidator').isValid()){
+       $.post('', $("#frmUser").serialize(), function (res) {
+           if (res.code == 200){
+               $("#tableUser").bootstrapTable('refresh');
+           }
+       });
+    }
 }
 
 function validate() {
     $("#frmCreate").bootstrapValidator({
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        excluded: [':disbled', ':hidden'],
+        message: "无效的值",
+        feedbackIcons: {/*input状态样式图片*/
+                 valid: 'glyphicon glyphicon-ok',
+                 invalid: 'glyphicon glyphicon-remove',
+                 validating: 'glyphicon glyphicon-refresh'
+             },
+        excuded: "disabled",
         fields: {
-
+            username: {
+                message: "输入的信息不正确",
+                validators: {
+                    notEmpty: {
+                        message: "工号不能为空"
+                    }
+                }
+            },
+            email: {
+                message: "输入的信息不正确",
+                validators: {
+                    message: "邮箱输入不正确",
+                    notEmpty: {
+                        message: "邮箱不能为空"
+                    },
+                    emailAddress: {
+                        message: "邮箱格式不正确"
+                    }
+                }
+            },
+            password: {
+                message: "输入的信息不正确",
+                validators: {
+                    message: "密码错误",
+                    notEmpty: {
+                        message: "密码不能为空"
+                    }
+                }
+            }
         }
     });
 }

e600719f28ebfc49d60a2f3a52de5057bc451e3d
cui <7211334@qq.com>
2018-06-01 18:33:55 +0800

    销售分析界面

--- storage/analysis_utils.py
+++ storage/analysis_utils.py
@@ -1,8 +1,9 @@
-import json
 
 
-def to_bar_chart_data(data):
-    """查询结果转化为图标可以使用的数据结构"""
+
+
+def to_bar_chart_data(table_data):
+    """查询结果转化为bar图标需要的格式
     labels = set([d[0] for d in data])
     products = set([d[2] for d in data])
     values = []
@@ -25,4 +26,42 @@ def to_bar_chart_data(data):
         "fields": list(products),
         "values": values
     }
-    return json.dumps(result)
+    return result
+    """
+    labels = [td[0] for td in table_data["rows"]]
+    fields = table_data["fields"][1:]
+    values = [[] for i in range(len(fields))]
+    for row in table_data["rows"]:
+        for i in range(len(fields)):
+            values[i].append(row[i+1])
+
+    return {
+        "labels": labels,
+        "fields": fields,
+        "values": values
+    }
+
+def to_table_data(data):
+    """转成表格数据"""
+    fields = ['日期'] + [f for f in set([d[2] for d in data])]
+    labels = set([d[0] for d in data])
+    values = []
+    x = 0
+    for lab in labels:
+        d1 = [dx for dx in data if dx[0] == lab]
+        values.append([])
+        y = 0
+        values[x].append(lab)
+        for p in fields[1:]:
+            d2 = [dy[1] for dy in d1 if dy[2] == p]
+            if len(d2) == 0:
+                values[x].append(0)
+            else:
+                values[x].append(d2[0])
+            y += 1
+        x += 1
+    return {
+        "fields": fields,
+        "rows": values
+    }
+
